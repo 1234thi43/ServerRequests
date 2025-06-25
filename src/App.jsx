3d9f516c68147f6edacb2d8cdbbffbce71fe786c
Danil <danilbystrov8@gmail.com>
2025-06-25 13:16:54 +0300

    add mock backend + loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,54 @@
 import { useEffect, useState } from 'react';
 import styles from './App.module.css';
 
+const PRODUCTS_MOCK = [
+	{
+		id: '0001',
+		name: 'Телевизор',
+		price: 10000,
+	},
+	{
+		id: '0002',
+		name: 'Телефон',
+		price: 5000,
+	},
+	{
+		id: '0003',
+		name: 'Компьютер',
+		price: 50000,
+	},
+];
+
 function App() {
 	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
-		fetch('https://mocki.io/v1/11a4ec3a-7513-4574-9b1d-813d06ca84a2')
+		setIsLoading(true);
+
+		new Promise((resolve) => {
+			setTimeout(() => {
+				resolve({ json: () => PRODUCTS_MOCK });
+			}, 5000);
+		})
 			.then((loadedData) => loadedData.json())
 			.then((loadedProducts) => {
 				setProducts(loadedProducts);
-			});
+			})
+			.finally(() => setIsLoading(false));
 	}, []);
 
 	return (
 		<div className={styles.app}>
-			{products.map(({ id, name, price }) => (
-				<div key={id}>
-					{name} - {price} руб.
-				</div>
-			))}
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				products.map(({ id, name, price }) => (
+					<div key={id}>
+						{name} - {price} руб.
+					</div>
+				))
+			)}
 		</div>
 	);
 }
