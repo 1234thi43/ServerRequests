7a4047ebd46c5f94196af6475851cb1bfeb74848
Danil <danilbystrov8@gmail.com>
2025-06-25 18:42:36 +0300

    add POST request and refrash

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,42 @@
 import { useEffect, useState } from 'react';
 import styles from './App.module.css';
 
-const PRODUCTS_MOCK = [
-	{
-		id: '0001',
-		name: 'Телевизор',
-		price: 10000,
-	},
-	{
-		id: '0002',
-		name: 'Телефон',
-		price: 5000,
-	},
-	{
-		id: '0003',
-		name: 'Компьютер',
-		price: 50000,
-	},
-];
-
 function App() {
 	const [products, setProducts] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [isCreating, setIsCreating] = useState(false);
+	const [refrashProductsFlag, setRefrashProductsFlag] = useState(false);
+
+	const refrashProducts = () => setRefrashProductsFlag(!refrashProductsFlag);
 
 	useEffect(() => {
 		setIsLoading(true);
 
-		new Promise((resolve) => {
-			setTimeout(() => {
-				resolve({ json: () => PRODUCTS_MOCK });
-			}, 5000);
-		})
+		fetch('http://localhost:3000/products')
 			.then((loadedData) => loadedData.json())
 			.then((loadedProducts) => {
 				setProducts(loadedProducts);
 			})
 			.finally(() => setIsLoading(false));
-	}, []);
+	}, [refrashProductsFlag]);
+
+	const requestAddPilesos = () => {
+		setIsCreating(true);
+		fetch('http://localhost:3000/products', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json; charset=utf-8' },
+			body: JSON.stringify({
+				name: 'Пылесоc',
+				price: 2009,
+			}),
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then((response) => {
+				console.log('Пылесос добавлен, ответ сервера: ', response);
+				refrashProducts();
+			})
+			.finally(() => setIsCreating(false));
+	};
 
 	return (
 		<div className={styles.app}>
@@ -50,6 +50,8 @@ function App() {
 					</div>
 				))
 			)}
+
+			<button disabled={isCreating} onClick={requestAddPilesos}>Добавить пылесос</button>
 		</div>
 	);
 }
