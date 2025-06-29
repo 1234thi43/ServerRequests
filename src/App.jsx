b44fcf724907c991014662e5fa709b0195994169
Danil <danilbystrov8@gmail.com>
2025-06-29 13:25:40 +0300

    DZ todo list with JSONplaseholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,32 @@
-// import viteLogo from '/vite.svg'
-// import { useState } from 'react';
-
-import { useRequestGetProducts } from './hooks/use-request-get-produts';
-import { useRequestAddPilesos } from './hooks/use-request-add-pilesos';
-import { useRequestUpdatePhone } from './hooks/use-request-update-phone';
-import { useRequestDeleteTv } from './hooks/use-request-delete-tv';
-
+import { useEffect, useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
-	// const [refrashProductsFlag, setRefrashProductsFlag] = useState(false);
+	const [todos, setTodos] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-	// const refrashProducts = () => setRefrashProductsFlag(!refrashProductsFlag);
+	useEffect(() => {
+		setIsLoading(true);
 
-	const { isLoading, products } = useRequestGetProducts();
-
-	const { isCreating, requestAddPilesos } = useRequestAddPilesos();
-	const { isUpdating, requestUpdatePhone } = useRequestUpdatePhone();
-	const { isDeleting, requestDeleteTv } = useRequestDeleteTv();
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((loadedData) => loadedData.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
 
 	return (
 		<div className={styles.app}>
 			{isLoading ? (
 				<div className={styles.loader}></div>
 			) : (
-				Object.entries(products).map(([id, { name, price }]) => (
-					<div key={id}>
-						{name} - {price} руб.
+				todos.map(({ id, title }) => (
+					<div className={styles.todo} key={id}>
+						Задача: {title}
 					</div>
 				))
 			)}
-
-			<button disabled={isCreating} onClick={requestAddPilesos}>
-				Добавить пылесос
-			</button>
-			<button disabled={isUpdating} onClick={requestUpdatePhone}>
-				Обновить телефон
-			</button>
-			<button disabled={isDeleting} onClick={requestDeleteTv}>
-				Удалить телевизор
-			</button>
 		</div>
 	);
 }
